02c3b212e0d84bd619c18708adf6aad0aa6cb836
DuttonMa <martin@theduttons.org>
2020-04-24 11:49:26 +0100

    reorganise config

--- web/sites/site.settings.php
+++ web/sites/site.settings.php
@@ -84,7 +84,7 @@ if (!isset($settings['file_public_path'])) {
 // Set up a config sync directory.
 //
 // This is defined inside the read-only "config" directory, deployed via Git.
-$settings['config_sync_directory'] = '../config/sync/' . $subsite_id;
+$settings['config_sync_directory'] = '../config/' . $subsite_id;
 
 // Local settings. These come last so that they can override anything.
 if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
