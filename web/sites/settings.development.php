8f68e4e9df0441149bc1bc86cb317eba92abc3a1
Johan Gant <johan.gant@gmail.com>
2020-04-27 15:33:42 +0100

    PHPCS fixes

--- web/sites/settings.development.php
+++ web/sites/settings.development.php
@@ -1,11 +1,15 @@
 <?php
 
-// Default Drupal 8 settings.
-//
-// These are already explained with detailed comments in Drupal's
-// default.settings.php file.
-//
-// See https://api.drupal.org/api/drupal/sites!default!default.settings.php/8
+/**
+ * @file
+ * Default Drupal 8 settings.
+ *
+ * These are already explained with detailed comments in Drupal's
+ * default.settings.php file.
+ *
+ * See https://api.drupal.org/api/drupal/sites!default!default.settings.php/8
+ */
+
 $settings['container_yamls'][] = $app_root . '/' . $site_path . '/development.services.yml';
 
 // Set config split environment.
