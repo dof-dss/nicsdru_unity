0280e8aa8ea69230bc5cae04b83f134f9544b3eb
omahm <hello@onemanandhismouse.com>
2020-06-17 15:07:06 +0100

    Update database creation script to use sites directory

--- .lando/scripts/create_databases.sh
+++ .lando/scripts/create_databases.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
-while read DB; do
-mysql -uroot -e "CREATE DATABASE IF NOT EXISTS $DB; \
-                 GRANT ALL PRIVILEGES ON $DB.* TO 'drupal8'@'%' IDENTIFIED by 'drupal8'; \
-                 CREATE DATABASE IF NOT EXISTS ${DB}_legacy; \
-                 GRANT ALL PRIVILEGES ON ${DB}_legacy.* TO 'drupal8'@'%' IDENTIFIED by 'drupal8';" | cut -f1 -d":";
-done < /app/lando.databases
+for dir in $(find /app/web/sites/ -mindepth 1 -maxdepth 1 -type d) ; do
+    db=${dir##*/} ;
+
+    if [ $db != 'default' ]; then
+          mysql -uroot -e "CREATE DATABASE IF NOT EXISTS $db; \
+                 GRANT ALL PRIVILEGES ON $db.* TO 'drupal8'@'%' IDENTIFIED by 'drupal8'; \
+                 CREATE DATABASE IF NOT EXISTS ${db}_legacy; \
+                 GRANT ALL PRIVILEGES ON ${db}_legacy.* TO 'drupal8'@'%' IDENTIFIED by 'drupal8';" | cut -f1 -d":";
+    fi
+done
