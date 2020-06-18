435ac7704484d1b82121182cd91e28ef75ed04ed
omahm <hello@onemanandhismouse.com>
2020-06-18 11:26:00 +0100

    Improve opt/flag parsing for import database

--- .lando/scripts/import_database.sh
+++ .lando/scripts/import_database.sh
@@ -1,11 +1,32 @@
 #!/usr/bin/env bash
 
-if [ -f "$2" ]; then
-    echo "Dropping $1 database"
-    mysql -u root -h $DB_HOST  -D $1 -e "DROP DATABASE IF EXISTS ${1};"
+for i in "$@"
+do
+case $i in
+    -d=*|--database=*)
+    database="${i#*=}"
+    shift # past argument=value
+    ;;
+    -f=*|--file=*)
+    file="${i#*=}"
+    shift # past argument=value
+    ;;
+    --default)
+    DEFAULT=YES
+    shift # past argument with no value
+    ;;
+    *)
+          # unknown option
+    ;;
+esac
+done
 
-    echo "Importing to $1"
-    mysql -u root -h $DB_HOST $1 < $2
+if [ -f "$file" ]; then
+    echo "Dropping ${database} database"
+    mysql -u root -h $DB_HOST  -D $database -e "DROP DATABASE IF EXISTS ${database};"
+
+    echo "Importing to $database"
+    mysql -u root -h $DB_HOST $database < $file
 else
-    echo "File '$2' does not exist. Aborting import"
+    echo "File '$file' does not exist. Aborting import"
 fi
