6c45a344940410b1696f0283236d088444f546ae
omahm <hello@onemanandhismouse.com>
2020-06-18 16:07:53 +0100

    Tidy up scripts and add comments

--- .lando/scripts/create_databases.sh
+++ .lando/scripts/create_databases.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 
+# Iterate every folder under web/sites and create a database with the default
+# Lando database credentials if it doesn't already exist.
 for dir in $(find /app/web/sites/ -mindepth 1 -maxdepth 1 -type d) ; do
-    db=${dir##*/} ;
+  database=${dir##*/} ;
 
-    if [ $db != 'default' ]; then
-          mysql -uroot -e "CREATE DATABASE IF NOT EXISTS $db; \
-                 GRANT ALL PRIVILEGES ON $db.* TO 'drupal8'@'%' IDENTIFIED by 'drupal8'; \
-                 CREATE DATABASE IF NOT EXISTS ${db}_legacy; \
-                 GRANT ALL PRIVILEGES ON ${db}_legacy.* TO 'drupal8'@'%' IDENTIFIED by 'drupal8';" | cut -f1 -d":";
-    fi
+  # Ignore the standard 'default' folder under web/sites.
+  if [[ ${database} != 'default' ]]; then
+     # Create the Drupal 8 and Drupal 7 legacy databases with the default Lando
+     # credentials if they don't already exist.
+     mysql -uroot -e "CREATE DATABASE IF NOT EXISTS $database; \
+                      GRANT ALL PRIVILEGES ON $database.* TO 'drupal8'@'%' IDENTIFIED by 'drupal8'; \
+                      CREATE DATABASE IF NOT EXISTS ${database}_legacy; \
+                      GRANT ALL PRIVILEGES ON ${database}_legacy.* TO 'drupal8'@'%' IDENTIFIED by 'drupal8';" | cut -f1 -d":";
+  fi
 done
