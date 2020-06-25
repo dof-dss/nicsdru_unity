bf9635161f7402b5172d6f0eb6d99da36038076e
DDWebDev04 <ddwebdev04@DDWebDev04s-MacBook-Pro.local>
2020-06-25 15:25:16 +0100

    code cleanup

--- web/modules/custom/unity_file_migrations/src/Plugin/migrate/process/DocumentEmbed.php
+++ web/modules/custom/unity_file_migrations/src/Plugin/migrate/process/DocumentEmbed.php
@@ -16,33 +16,37 @@ use GuzzleHttp\Exception\RequestException;
  *  id = "document_embed"
  * )
  */
-class DocumentEmbed extends ProcessPluginBase {
+class DocumentEmbed extends ProcessPluginBase
+{
 
-  /**
-   * @param $value
-   *  Body Value.
-   * @param MigrateExecutableInterface $migrate_executable
-   *  Migrate Executable.
-   * @param Row $row
-   *  Row.
-   * @param $destination_property
-   *  Destination Property.
-   * @return array|string|string[]|null
-   *  Body Value return.
-   */
-  public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
+    /**
+     * @param  $value
+     *  Body Value.
+     * @param  MigrateExecutableInterface $migrate_executable
+     *  Migrate Executable.
+     * @param  $row
+     *  Row.
+     * @param  $destination_property
+     *  Destination Property.
+     * @return array|string|string[]|null
+     *  Body Value return.
+     */
+    public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property)
+    {
 
-    // Publication Page link used for replacing embedded links.
-    $publications_link = Link::createFromRoute('Publication Page',
-      'entity.node.canonical',
-      ['node' => 2593],
-      ['attributes' => ['rel' => 'nofollow', 'class' => 'publication_link']])->toString();
+        // Publication Page link used for replacing embedded links.
+        $publications_link = Link::createFromRoute(
+            'Publication Page',
+            'entity.node.canonical',
+            ['node' => 2593],
+            ['attributes' => ['rel' => 'nofollow', 'class' => 'publication_link']]
+        )->toString();
 
-    // Extract and replace links.
-    $embed_regex = '/<a[\w\s\.]*href="([\w:\-\/\.]*)(pdf|doc|doxc)[\w\s\.\=\-"\':><(&);\/]+<\/a>/U';
-    $value = preg_replace($embed_regex, $publications_link, $value);
+        // Extract and replace links.
+        $embed_regex = '/<a[\w\s\.]*href="([\w:\-\/\.]*)(pdf|doc|doxc)[\w\s\.\=\-"\':><(&);\/]+<\/a>/U';
+        $value = preg_replace($embed_regex, $publications_link, $value);
 
-    return $value;
-  }
+        return $value;
+    }
 
 }
