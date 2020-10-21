9f9075f5783c996654722186fffec5fb27518a8b
DuttonMa <martin@theduttons.org>
2020-10-21 11:40:09 +0100

    make absolute link filter more generic

--- web/modules/custom/unity_file_migrations/src/Plugin/migrate/process/AbsoluteLinkFilter.php
+++ web/modules/custom/unity_file_migrations/src/Plugin/migrate/process/AbsoluteLinkFilter.php
@@ -30,9 +30,9 @@ class AbsoluteLinkFilter extends ProcessPluginBase {
           $matches2 = [];
           if (preg_match('|http://(.*)|', $original_link, $matches2) ||
             preg_match('|https://(.*)|', $original_link, $matches2)) {
-            // This is an absolute URL.
-            // Does it look like a self link to Uregni ?
-            $this->convertUregniLink($matches2, $value, $original_link);
+            // This is an absolute URL does it match one
+            // of the parameters to this plugin?
+            $this->convertAbsoluteLink( $value, $original_link);
           }
         }
       }
@@ -41,43 +41,23 @@ class AbsoluteLinkFilter extends ProcessPluginBase {
   }
 
   /**
-   * Utility function to convert internal Uregni link to relative.
+   * Utility function to convert internal link to relative.
    */
-  private function convertUregniLink($matches, &$value, $original_link) {
-    if (count($matches) > 1) {
-      $remaining_url_portion = $matches[1];
-      // Get site name to search for and replace.
-      if (!isset($this->configuration['site_name'])) {
-        throw new MigrateException('"site_name" must be configured.');
-      }
-      // Does the domain name contain the site name ?
-      $regex = '|[^\/]*' . $this->configuration['site_name'] . '[^\/]*|';
-      if (preg_match($regex, $remaining_url_portion)) {
-        // This is a self link to <site_name> that has been entered as an
-        // absolute path so convert it into a relative path.
-        // To do this, just take everything from the first '/'.
-        $matches2 = [];
-        if (preg_match('|[^ \/]*([\/])(.*)|', $remaining_url_portion, $matches2)) {
-          if (count($matches2) > 2) {
-            $relative_link = '/' . $matches2[2];
-            // Get specified patterns to search for and replace.
-            $replace = 'files/' . $this->configuration['site_name'];
-            if (isset($this->configuration['test_url_1'])) {
-              // Catch this alternative format too.
-              $regex = 'sites/' . $this->configuration['test_url_1'] . '/files';
-              $relative_link = str_replace($regex, $replace, $relative_link);
-            }
-            // Now we just need to convert /sites/<site_name>/files location
-            // to /files/<site_name> for D8.
-            $regex = 'sites/' . $this->configuration['site_name'] . '/files';
-            $relative_link = str_replace($regex, $replace, $relative_link);
-            // Now replace all occurences of the original absolute link
-            // with the new relative link.
-            $value['value'] = str_replace($original_link, $relative_link, $value['value']);
-          }
-        }
+  private function convertAbsoluteLink(&$value, $original_link) {
+    // Get list of urls to replace.
+    if (!isset($this->configuration['replace_urls'])) {
+      throw new MigrateException('"replace_urls" must be configured.');
+    }
+    $replace_url_list = explode(" ", $this->configuration['replace_urls']);
+    foreach ($replace_url_list as $replace_url) {
+      // Does the start of this link match one of the
+      // urls that we are looking for ?
+      if (strpos($original_link, $replace_url) == 0) {
+        // We have a match, change this absolute link
+        // to a relative link.
+        $value['value'] = str_replace($replace_url, '', $value['value']);
+        $this->messenger()->addWarning("Replacing " . $original_link);
       }
     }
   }
-
 }
