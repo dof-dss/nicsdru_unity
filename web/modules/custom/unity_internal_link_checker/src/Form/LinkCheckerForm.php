eef3abf6d18a0d148bdabff84dbea52c06ad282e
DuttonMa <martin@theduttons.org>
2020-11-10 12:11:01 +0000

    remove unnecessary code

--- web/modules/custom/unity_internal_link_checker/src/Form/LinkCheckerForm.php
+++ web/modules/custom/unity_internal_link_checker/src/Form/LinkCheckerForm.php
@@ -15,54 +15,6 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 class LinkCheckerForm extends ConfigFormBase {
 
   /**
-   * The entity type manager.
-   *
-   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
-   */
-  protected $entityTypeManager;
-
-  /**
-   * A logger instance.
-   *
-   * @var \Psr\Log\LoggerInterface
-   */
-  protected $logger;
-
-  /**
-   * The messenger service.
-   *
-   * @var \Drupal\Core\Messenger\MessengerInterface
-   */
-  protected $messenger;
-
-  /**
-   * Creates a new AuditSettingsForm instance.
-   *
-   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
-   *   The entity type manager.
-   * @param \Psr\Log\LoggerInterface $logger
-   *   The logger interface.
-   * @param \Drupal\Core\Messenger\MessengerInterface $messenger
-   *   The messenger service.
-   */
-  public function __construct(EntityTypeManagerInterface $entity_type_manager, LoggerInterface $logger, MessengerInterface $messenger) {
-    $this->entityTypeManager = $entity_type_manager;
-    $this->logger = $logger;
-    $this->messenger = $messenger;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function create(ContainerInterface $container) {
-    return new static(
-      $container->get('entity_type.manager'),
-      $container->get('logger.factory')->get('nics_audit_settings_form'),
-      $container->get('messenger')
-    );
-  }
-
-  /**
    * {@inheritdoc}
    */
   protected function getEditableConfigNames() {
