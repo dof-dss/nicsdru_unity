89b820c4de6eea553bc4924b210dc6bf35d5598b
DuttonMa <martin@theduttons.org>
2021-01-22 15:49:32 +0000

    new facet drilldown contrib module

---
+++ web/modules/custom/date_facet_drilldown/src/Plugin/facets/processor/DateFacetDrilldownProcessor.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Drupal\date_facet_drilldown\Plugin\facets\processor;
+
+use Drupal\Core\Datetime\DrupalDateTime;
+use Drupal\facets\FacetInterface;
+use Drupal\facets\Processor\BuildProcessorInterface;
+use Drupal\facets\Processor\ProcessorPluginBase;
+use Drupal\facets\Result\Result;
+
+/**
+ * Provides a processor that formats date facets.
+ *
+ * @FacetsProcessor(
+ *   id = "date_facet_drilldown",
+ *   label = @Translation("Date facet drilldown"),
+ *   description = @Translation("Date facets - allow drilldown to month from year"),
+ *   stages = {
+ *     "build" = 1
+ *   }
+ * )
+ */
+class DateFacetDrilldownProcessor extends ProcessorPluginBase implements BuildProcessorInterface {
+  public function build(FacetInterface $facet, array $results) {
+    $month_facets = FALSE;
+    foreach ($results as $id => $result) {
+      if (preg_match('/^\d{4}-\d{2}$/', $id)) {
+        // Facet key format is YYYY-MM, so we need to format
+        // this as full month name followed by year
+        // e.g. 'January 2020'.
+        $dateTime = new DrupalDateTime();
+        $facet_date = $dateTime::createFromFormat('Y-m-d\TH:i:s', $id . '-01T00:00:00');
+        $result->setDisplayValue($facet_date->format('F Y'));
+        $month_facets = TRUE;
+      }
+    }
+    if ($month_facets) {
+      // In this case we are drilled down to the month level and
+      // need to create an additional active year facet.
+      $facetActiveItems = $facet->getActiveItems();
+      $activeItem = array_pop($facetActiveItems);
+      $this->createActiveFacet($facet, $activeItem, $results);
+    }
+    return $results;
+  }
+
+  /**
+   * Utility function.
+   *
+   * Create an active year facet.
+   *
+   * @param $facet
+   * @param $activeItem
+   * @param $results
+   */
+  private function createActiveFacet($facet, $activeItem, &$results) {
+    $matches = [];
+    // Take YYYY from the start of the active item.
+    if (preg_match('/^(\d{4})/', $activeItem, $matches)) {
+      $year = $matches[0];
+      // Create new YYYY active facet.
+      $results[$year] = new Result($facet, $year, $year, 0);
+      $results[$year]->setActiveState(TRUE);
+    }
+  }
+
+}
---
+++ web/modules/custom/date_facet_drilldown/src/Plugin/facets/query_type/SearchApiDateFacetDrilldown.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Drupal\date_facet_drilldown\Plugin\facets\query_type;
+
+use Drupal\Core\Datetime\DrupalDateTime;
+use Drupal\facets\QueryType\QueryTypeRangeBase;
+
+/**
+ * Support for date facets within the Search API scope.
+ *
+ * This query type supports dates for all possible backends. This specific
+ * implementation of the query type supports a generic solution of adding facets
+ * for dates.
+ *
+ * @FacetsQueryType(
+ *   id = "search_api_datefacetdrilldown",
+ *   label = @Translation("Date facet drilldown"),
+ * )
+ */
+class SearchApiDateFacetDrilldown extends QueryTypeRangeBase {
+
+  public function __construct(array $configuration, $plugin_id, $plugin_definition) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+  }
+
+  /**
+   * Returns a start and end date based on a unix timestamp.
+   *
+   * This method returns a start and end date with an absolute interval, based
+   * on the granularity set in the widget.
+   *
+   * @param int $value
+   *   Unix timestamp.
+   *
+   * @return array
+   *   An array with a start and end date as unix timestamps.
+   *
+   * @throws \Exception
+   *   Thrown when creating a date fails.
+   */
+  public function calculateRange($value) {
+    $dateTime = new DrupalDateTime();
+    if (preg_match('/^\d{4}$/', $value)) {
+      // This must be a year (YYYY).
+      $startDate = $dateTime::createFromFormat('Y-m-d\TH:i:s', $value . '-01-01T00:00:00');
+      $stopDate = $dateTime::createFromFormat('Y-m-d\TH:i:s', $value . '-12-31T23:59:59');
+    } else {
+      // This must be a year and month (YYYY-MM).
+      $startDate = $dateTime::createFromFormat('Y-m-d\TH:i:s', $value . '-01T00:00:00');
+      // Use format('t') to retrieve the number of days in the month.
+      $stopDate = $dateTime::createFromFormat('Y-m-d\TH:i:s', $value . '-' . $startDate->format('t') . 'T23:59:59');
+    }
+    return [
+      'start' => $startDate->format('U'),
+      'stop' => $stopDate->format('U'),
+    ];
+  }
+
+  /**
+   * Calculates the result of the filter.
+   *
+   * @param int $value
+   *   A unix timestamp.
+   *
+   * @return array
+   *   An array with a start and end date as unix timestamps.
+   */
+  public function calculateResultFilter($value) {
+    // Create a new DrupalDateTime from the value passed in.
+    $date = new DrupalDateTime();
+    $date->setTimestamp($value);
+
+    // Decide whether to format the facets as years or
+    // years/months depending on how many facets are active.
+    $active_items = $this->facet->getActiveItems();
+    if (isset($active_items) && (count($active_items) > 0)) {
+      $format = 'Y-m';
+    } else {
+      $format = 'Y';
+    }
+
+    return [
+      'display' => $date->format($format),
+      'raw' => $date->format($format)
+    ];
+  }
+
+}
