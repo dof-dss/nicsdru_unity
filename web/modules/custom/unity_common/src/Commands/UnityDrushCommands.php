47800932761ae2817d7c7962821033964511672c
DuttonMa <martin@theduttons.org>
2021-02-09 12:31:58 +0000

    created custom drush command

---
+++ web/modules/custom/unity_common/src/Commands/UnityDrushCommands.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Drupal\unity_common\Commands;
+
+use Drush\Commands\DrushCommands;
+use \Drupal\structure_sync\StructureSyncHelper;
+
+/**
+ * Drush custom commands.
+ */
+class UnityDrushCommands extends DrushCommands {
+  /**
+   * Drush command import blocks, taxonomies and menus  using structure_sync.
+   *
+   * @param string $option
+   *  Argument to select 'safe', 'full' or 'force'
+   *
+   * @command import_all_if_installed
+   */
+  public function import_all_if_installed($option = 'safe') {
+    // Only import if the structure_sync module is installed.
+    if (\Drupal::moduleHandler()->moduleExists('structure_sync')) {
+      // Import blocks.
+      StructureSyncHelper::importCustomBlocks([
+        'style' => $option,
+        'drush' => TRUE,
+      ]);
+      // Import taxonomies.
+      StructureSyncHelper::importTaxonomies([
+        'style' => $option,
+        'drush' => TRUE,
+      ]);
+      // Import menus.
+      StructureSyncHelper::importMenuLinks([
+        'style' => $option,
+        'drush' => TRUE,
+      ]);
+    }
+  }
+}
