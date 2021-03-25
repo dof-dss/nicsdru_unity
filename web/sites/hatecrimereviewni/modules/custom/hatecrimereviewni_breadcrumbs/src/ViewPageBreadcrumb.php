dd183076cfa6e7c4f6129bff03512cdab7029b8c
Adam Williamson <adam.williamson@finance-ni.gov.uk>
2021-03-25 15:20:59 +0000

    Custom module for breadcrumbs

---
+++ web/sites/hatecrimereviewni/modules/custom/hatecrimereviewni_breadcrumbs/src/ViewPageBreadcrumb.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace Drupal\hatecrimereviewni_breadcrumbs;
+
+/**
+ * @file
+ * Generates the breadcrumb trail for search page(s)
+ *
+ * In the format:
+ * > Home
+ * > current-page-title
+ *
+ * > <front>
+ * > /current-page-title
+ */
+use Drupal\Core\Breadcrumb\Breadcrumb;
+use Drupal\Core\Breadcrumb\BreadcrumbBuilderInterface;
+use Drupal\Core\Controller\TitleResolverInterface;
+use Drupal\Core\Routing\RouteMatchInterface;
+use Drupal\Core\Link;
+use Drupal\Core\Url;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+/**
+ * {@inheritdoc}
+ */
+class ViewPageBreadcrumb implements BreadcrumbBuilderInterface {
+
+  /**
+   * Symfony\Component\HttpFoundation\RequestStack definition.
+   *
+   * @var Symfony\Component\HttpFoundation\RequestStack
+   */
+  protected $request;
+
+  /**
+   * The title resolver.
+   *
+   * @var \Drupal\Core\Controller\TitleResolverInterface
+   */
+  protected $titleResolver;
+
+  /**
+   * Class constructor.
+   */
+  public function __construct(RequestStack $request, TitleResolverInterface $title_resolver) {
+    $this->request = $request;
+    $this->titleResolver = $title_resolver;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('request_stack'),
+      $container->get('title_resolver')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function applies(RouteMatchInterface $route_match) {
+    $match = FALSE;
+    $route_name = $route_match->getRouteName();
+    $view_names = [
+      'view.news_search_api.news_search_page',
+    ];
+
+    foreach ($view_names as $view_name) {
+      if ($route_name == $view_name) {
+        $match = TRUE;
+      }
+    }
+    return $match;
+
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build(RouteMatchInterface $route_match) {
+    $route_name = $route_match->getRouteName();
+    $breadcrumb = new Breadcrumb();
+    $title_resolver = $this->titleResolver->getTitle($this->request->getCurrentRequest(), $route_match->getRouteObject());
+    $links[] = Link::createFromRoute(t('Home'), '<front>');
+    $links[] = Link::createFromRoute($title_resolver, '<none>');
+    $breadcrumb->setLinks($links);
+    $breadcrumb->addCacheContexts(['url.path']);
+
+    return $breadcrumb;
+  }
+
+}
