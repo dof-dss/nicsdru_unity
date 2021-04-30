7c492f2aff3df1a59528749cd4d56a27bdb2b0d7
David Corbett <ideetechnology@gmail.com>
2021-04-30 03:04:52 +0100

    make another tweak to the sites.platform as the getupstreamroutes function doesn't seem to work

--- web/sites/sites.platformsh.php
+++ web/sites/sites.platformsh.php
@@ -17,13 +17,31 @@ if (!$platformsh->inRuntime()) {
 // Note also that the corresponding directory in sites will be just up to the first dot
 // in the domain name, so the route "https://uregni.gov.uk.{default}/" will correspond
 // to the sites/uregni directory.
-foreach ($platformsh->getUpstreamRoutes($platformsh->applicationName) as $route) {
-  $host = parse_url($route['url'], PHP_URL_HOST);
-  if ($host !== FALSE) {
-    // host is www.fiscalcommissionni.org
-    // host is fiscalcommissionni.org.master-7rqtwti-6tlkpwbr6tndk.uk-1.platformsh.site
-    $newhost = str_replace('www.','',$host);
-    $subdomain = substr($host, 0, strpos($newhost, '.'));
-    $sites[$host] = $subdomain;
+//foreach ($platformsh->getUpstreamRoutes($platformsh->applicationName) as $route) {
+//  $host = parse_url($route['url'], PHP_URL_HOST);
+//  if ($host !== FALSE) {
+//    // host is www.fiscalcommissionni.org
+//    // host is fiscalcommissionni.org.master-7rqtwti-6tlkpwbr6tndk.uk-1.platformsh.site
+//    $newhost = str_replace('www.','',$host);
+//    $subdomain = substr($host, 0, strpos($newhost, '.'));
+//    $sites[$host] = $subdomain;
+//  }
+//}
+
+if (isset($_ENV['PLATFORM_ROUTES'])) {
+  $routes = json_decode(base64_decode($_ENV['PLATFORM_ROUTES']), TRUE);
+  if (!empty($routes) && is_array($routes)) {
+    foreach ($routes as $key => $route) {
+      if ($route['type'] == 'upstream') {
+        $host = parse_url($key, PHP_URL_HOST);
+        if ($host !== FALSE) {
+          // host is www.fiscalcommissionni.org
+          // host is fiscalcommissionni.org.master-7rqtwti-6tlkpwbr6tndk.uk-1.platformsh.site
+          $newhost = str_replace('www.','',$host);
+          $subdomain = substr($host, 0, strpos($newhost, '.'));
+          $sites[$host] = $subdomain;
+        }
+      }
+    }
   }
 }
