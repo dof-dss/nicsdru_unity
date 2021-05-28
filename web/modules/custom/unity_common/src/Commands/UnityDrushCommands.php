fdceeea8f1204e7038e3f7649782c5b0dd92f255
Adam Williamson <adam.williamson@finance-ni.gov.uk>
2021-05-28 10:58:04 +0100

    Drush command to update the starterkit with new theme name

--- web/modules/custom/unity_common/src/Commands/UnityDrushCommands.php
+++ web/modules/custom/unity_common/src/Commands/UnityDrushCommands.php
@@ -39,4 +39,45 @@ class UnityDrushCommands extends DrushCommands {
     }
   }
 
+  /**
+   * Drush command replace SITE_FOLDER_NAME and STARTERKIT text in a unity site sub-theme.
+   *
+   * @param string $site_folder_name
+   *   Argument for user to enter the site directory they wish to target.
+   * @param string $starterkit
+   *   Argument for user to add the new name of the sub-theme, replacing STARTERKIT.
+   * @command update-starterkit-text
+   * @aliases upst
+   * @usage update-starterkit-text --site_folder_name --new_theme_name
+   */
+  public function changeText($site_folder_name = '', $starterkit = '') {
+    // Get the site folder name entered as the 1st parameter by the user.
+    $site_path = getcwd() . '/sites/' . $site_folder_name;
+
+    // Rename the STARTERKIT directory to the new sub-theme name entered by the user as the 2nd parameter.
+    rename($site_path . '/themes/STARTERKIT', $site_path . '/themes/' . $starterkit);
+    $new_theme_path = $site_path . '/themes/' . $starterkit;
+
+    // Change the theme file names.
+    $theme_file_names = glob($new_theme_path . '/*');
+    foreach ($theme_file_names as $theme_file_name) {
+      $new_file_name = str_replace('STARTERKIT', $starterkit, $theme_file_name);
+      rename($theme_file_name, $new_file_name);
+    }
+
+    // Loop through the new theme directories and update any STARTERKIT and SITE_DIRECTORY_TEXT text.
+    $site_directories = [glob($new_theme_path . '/config/*'), glob($new_theme_path . '/*')];
+    foreach ($site_directories as $site_directory) {
+      foreach ($site_directory as $file) {
+        $file_contents = file_get_contents($file);
+        $strings = ['STARTERKIT', 'SITE_DIRECTORY_NAME'];
+        $string_replacements = [$starterkit, $site_folder_name];
+        $file_contents = str_replace($strings, $string_replacements, $file_contents);
+        if (!is_dir($file)) {
+          file_put_contents($file, $file_contents);
+        }
+      }
+    }
+  }
+
 }
