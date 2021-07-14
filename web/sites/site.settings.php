0622efa1b37ed52650718afb080939e9d27e4a43
DuttonMa <martin@theduttons.org>
2021-07-14 13:14:22 +0100

    Use production config split for main branch

--- web/sites/site.settings.php
+++ web/sites/site.settings.php
@@ -64,7 +64,7 @@ if (!empty(getenv('PLATFORM_BRANCH'))) {
     include $app_root . '/' . $site_path . '/../settings.platformsh.php';
   }
 
-  // For now, use 'hosted' config split for all Platform.sh sites.
+  // Use 'hosted' config split for all Platform.sh sites apart from production.
   $config['config_split.config_split.hosted']['status'] = TRUE;
 
   // Environment specific settings and services.
@@ -72,6 +72,8 @@ if (!empty(getenv('PLATFORM_BRANCH'))) {
     case 'main':
       // De-facto production settings.
       $settings['container_yamls'][] = $app_root . '/' . $site_path . '/../services.yml';
+      // Use 'production' config split for the Platform.sh production site.
+      $config['config_split.config_split.production']['status'] = TRUE;
       break;
 
     case 'D8UN-edge':
