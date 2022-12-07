98f306532c77c1e0d78762951094210fda2dbcbc
DuttonMa <martin@theduttons.org>
2022-12-07 14:02:46 +0000

    remove local settings

--- project/sites/employmenttribunalsni/settings.local.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$config['config_split.config_split.local']['status'] = TRUE;
-$config['config_split.config_split.hosted']['status'] = FALSE;
-$config['config_split.config_split.production']['status'] = FALSE;
